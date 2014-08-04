1ecf41b62f5500e46de7f82469c700b4b7c57bd8
icehockey38 <dillonhammond@gmail.com>
2014-08-04 11:32:20 -0500

    Created Xcode Project

---
+++ OpenGL Testing/OpenGL Testing/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGL Testing
+//
+//  Created by Dillon Hammond on 8/4/14.
+//  Copyright (c) 2014 Learning. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
