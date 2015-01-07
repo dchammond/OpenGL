e0313ad2657caa423f5c33a04f0167ea86c4d2b4
Dillon Hammond <dillonhammond@gmail.com>
2015-01-07 17:02:55 -0600

    Added project

---
+++ Learning/Learning/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Learning
+//
+//  Created by Dillon Hammond on 1/7/15.
+//  Copyright (c) 2015 Dillon Hammond. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
